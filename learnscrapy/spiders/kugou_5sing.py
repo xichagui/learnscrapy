d622042a83ef69bb0134b084fe2950c9c218bef5
xichagui <xichagui@gmail.com>
2017-05-08 16:47:49 +0800

    增加随机浏览器请求头user-agent中间件

--- learnscrapy/spiders/kugou_5sing.py
+++ learnscrapy/spiders/kugou_5sing.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
+import logging
 import scrapy
 
 
 class Kugou5singSpider(scrapy.Spider):
     name = "kugou_5sing"
     allowed_domains = ["kugou.com"]
-    start_urls = ['http://kugou.com/']
+    start_urls = ['http://5sing.kugou.com/tenderjun/default.html',
+                  'http://5sing.kugou.com/tenderjun/default.html',
+                  'http://5sing.kugou.com/',
+                  ]
 
     def parse(self, response):
-        pass
+        logging.info('status code is %d', response.status)
